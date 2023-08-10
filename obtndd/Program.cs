7eaa10373f67e5ef91f53e576476539ab2fce2fc
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-10 19:53:13 -0300

    Commit

--- obtndd/Program.cs
+++ obtndd/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Quem é o estudante de C# ");
+        Console.WriteLine("Quem é o estudante de C# mais GOSTOSO");
         var nprof = Console.ReadLine();
-        Console.WriteLine($"O estudante mais gostoso do sharp é o{nprof}");
+        Console.WriteLine($"O estudante mais gostoso do sharp é o {nprof}");
     }
 }
