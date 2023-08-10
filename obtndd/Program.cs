c0aae0f26fe75cd8dd7b9d65485912778b919c49
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-10 18:52:41 -0300

    Second commit
    Primeira concatenação e uso de var em c#

---
+++ obtndd/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace obtndd;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quem é o estudante de C# ");
+        var nprof = Console.ReadLine();
+        Console.WriteLine($"O estudante mais gostoso do sharp é o{nprof}");
+    }
+}
