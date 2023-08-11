efe9c6353f5923608bb9481a053a293ad71a6e74
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-11 15:27:27 -0300

    Alterações no exercicios, e teste de depuração e debug

--- obtndd/Program.cs
+++ obtndd/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Quem é o estudante de C# mais GOSTOSO");
+        Console.Write("Digite o nome do professor: ");
         var nprof = Console.ReadLine();
-        Console.WriteLine($"O estudante mais gostoso do sharp é o {nprof}");
+        var nomemaiuscl = nprof!.ToUpper();
+        Console.WriteLine($"O nome do professor é {nomemaiuscl}.");
     }
 }
