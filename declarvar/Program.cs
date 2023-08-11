e6237806e46042237c4d36c990ae63ece1182065
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-11 15:54:15 -0300

    first commit in "tpchar"

---
+++ declarvar/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace declarvar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String aluno;
+        aluno = "Renan de Oliveira Gomes";
+        String alunom = aluno.ToUpper();
+        Console.WriteLine($"O nome do aluno é {alunom}");
+    }
+}
---
+++ tpchar/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace tpchar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
