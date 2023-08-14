a8a5b24cf22c222a5ce2ad48aa4a14d1b35d5105
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-13 21:22:04 -0300

    Novo exercicio usando var(char)

--- tpchar/Program.cs
+++ tpchar/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //char VCaractereb = 'b';
+        char VCaracterB = 'B';
+        //char VCaractera = 'a';
+        char VCaracterA = 'A';
+        var teste = VCaracterA + VCaracterB;
+        System.Console.WriteLine($"A soma dos valres numericos dos valores de char é: {teste}");
+
+        /*char letra;
+        letra = 'b';
+        System.Console.WriteLine($"Esta é meu primeiro exemplo usando tipo char, adicionei o valor: {letra}");*/
     }
 }
