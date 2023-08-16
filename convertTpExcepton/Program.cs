20d24f7502e39e5db749bca4216223211ce6f418
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-16 16:16:52 -0300

    Exercicios de exception

---
+++ convertTpExcepton/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace convertTpExcepton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 97;
+        char y;
+
+        try
+        {
+            y = Convert.ToChar(x);
+            System.Console.WriteLine($"O valor {x} do tipo {x.GetType()} foi convertido para o valor {y} do tipo {y.GetType()}");
+        }
+        catch(FormatException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+        }
+        catch(OverflowException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+            //Value was either too large or too small for an Int32.: 58182810050414
+        }
+        catch(InvalidCastException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+        }
+    }
+}
---
+++ convertTpNumClssConvert/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace convertTpNumClssConvert;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 97.50;
+        int y = Convert.ToInt32(x);
+        System.Console.WriteLine($"O valor {x} do tipo {x.GetType()} foi convertido para o valor {y} do tipo {y.GetType()}");
+    }
+}
