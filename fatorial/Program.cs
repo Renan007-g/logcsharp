5cb38cd5e23d2ac9dea7a79ac34026e994b22910
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-17 17:41:29 -0300

    Num fatorial

---
+++ fatorial/Program.cs
@@ -0,0 +1,66 @@
+﻿/*namespace fatorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Digite um numero inteiro positivo: ");
+        int num = Int32.Parse(Console.ReadLine());
+        /*Console.ReadLine();
+        int num = Convert.ToInt32(Console.ReadLine());*/
+
+        /*if (num < 0)
+        {
+            System.Console.WriteLine("O numero deve ser positivo");
+        }
+        else
+        {
+            int fatorial =CalcularFatorial(num);
+            Console.WriteLine($"O fatorial do numero {num} é igual a: {fatorial}");
+        }
+    }
+    static int CalcularFatorial(int n)
+    {
+        if(n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * CalcularFatorial(n-1);
+        }
+    }
+}*///Meu codigo
+namespace CalculadoraFatorial//Codigo GPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro não negativo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("O número deve ser não negativo.");
+            }
+            else
+            {
+                int fatorial = CalcularFatorial(numero);
+                Console.WriteLine($"O fatorial de {numero} é: {fatorial}");
+            }
+        }
+
+        static int CalcularFatorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalcularFatorial(n - 1);
+            }
+        }
+    }
+}
