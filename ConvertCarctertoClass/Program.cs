98e337e41ebd46b118e3c0083539bc06c43e6215
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-17 15:19:35 -0300

    Novo exercicio feito

---
+++ ConvertCarctertoClass/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConvertCarctertoClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string x = "B";
+        char y;
+
+        try
+        {
+            y = Convert.ToChar(x);
+            System.Console.WriteLine($"O valor {x} do tipo {x.GetType()} foi convertido para o valor {y} do tipo {y.GetType()}");
+        }
+        catch(FormatException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+        }
+        catch(OverflowException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+        }
+        catch(InvalidCastException e)
+        {
+            System.Console.WriteLine($"{e.Message}: {x}");
+        }
+    }
+}
---
+++ ConvertTpBool/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConvertTpBool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 0;
+        bool y = Convert.ToBoolean(x);
+        System.Console.WriteLine($"O valor {x} do tipo {x.GetType()} foi Convertido para o valor {y} do tipo {y.GetType}. ");
+    }
+}
