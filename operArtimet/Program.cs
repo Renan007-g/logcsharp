cb30af14b179a064bc14ba0fee9dd62ac1ef87c8
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-18 16:39:35 -0300

    Exercicio de operadores aritimeticos

---
+++ operArtimet/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace operArtimet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Digite um numero: ");
+        int valorA = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Digite outro numero: ");
+        int valorB = Convert.ToInt32(Console.ReadLine()) ;
+
+        int res = valorA + valorB;
+        System.Console.WriteLine($"A soma entre {valorA} e {valorB} é igual a {res}");
+    }
+}
