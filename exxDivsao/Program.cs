273d5bd4944c4d58c3cf051fd73cb75dd6747451
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-21 16:12:09 -0300

    Exercicio de Media aritimetica

---
+++ exxDivsao/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exxDivsao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("###Divisão###");
+
+        System.Console.WriteLine("Digite o dividendo: ");
+        decimal dividendo = Convert.ToDecimal(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o divisor: ");
+        decimal divisor = Convert.ToDecimal(Console.ReadLine());
+
+        decimal quociente = dividendo / divisor;
+        System.Console.WriteLine($"O a divisão entre {dividendo} e {divisor} é igual a {quociente}");
+    }
+}
---
+++ exxMedia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace exxMedia;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float m1;
+        float m2;
+        float m3;
+
+        System.Console.WriteLine("Digite a nota 1: ");
+        m1 = Convert.ToSingle(Console.ReadLine());
+
+        System.Console.WriteLine("Digite a nota 2: ");
+        m2 = Convert.ToSingle(Console.ReadLine());
+
+        System.Console.WriteLine("Digite a nota 3: ");
+        m3 = Convert.ToSingle(Console.ReadLine());
+
+        float mediaTotal = (m1 + m2 + m3) / 3;
+        System.Console.WriteLine($"A média total entre os valores {m1}, {m2} e {m3} é de {mediaTotal}");
+
+    }
+}
