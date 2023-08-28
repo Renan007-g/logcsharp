b880122564869bc7e176ba481ab13ea10f3e87c6
Renan007-g <renan.oliv.2007@gmail.com>
2023-08-28 16:41:45 -0300

    Exrcicio de while

---
+++ exxTabCMulti/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exxTabCMulti;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        int mult;
+        int tab;
+
+        System.Console.WriteLine("Digite um multiplicando para a tabuada");
+        num = Convert.ToInt32(Console.ReadLine());
+        mult = 0;
+        
+        
+        while(mult <= 10)
+        {
+            tab = num * mult;
+            System.Console.WriteLine($"{num} x {mult} = {tab}");
+            mult += 1;
+        }
+    }
+}
---
+++ exxWhleNumPar/Program.cs
@@ -0,0 +1,7 @@
+﻿int cont = 0;
+
+while(cont <= 100)
+{
+    System.Console.WriteLine(cont);
+    cont += 2;
+}
