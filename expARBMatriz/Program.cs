21d55b2cfdb59ebcc85707c314ff8088f6ce2af1
Renan007-g <renan.oliv.2007@gmail.com>
2023-09-29 13:29:14 -0300

    COmmit array

---
+++ expARBMatriz/Program.cs
@@ -0,0 +1,36 @@
+﻿bool[,] CineCadeiras = new bool[5,4];//Aula 137
+
+for(int i = 0; i < 5; i++)//Laço externo representando linhas
+{
+    for(int j = 0; j < 4; j++)//laço interno representando colunas
+    {
+        CineCadeiras[i, j] = true;
+    }
+    
+}
+
+//Ou ao contrario:
+
+/*for(int j = 0; j < 4; j++)//Laço externo representando linhas
+{
+    for(int i = 0; i < 5; i++)//laço interno representando colunas
+    {
+        CineCadeiras[i, j] = true;
+    }              //L  C
+                   //i  o
+                   //n  l
+                   //h  u
+                   //a  n
+                   //   a
+}*/
+
+for(int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 4; j++)
+    {
+        System.Console.Write($"{CineCadeiras[i, j]}\t ");
+    }
+    System.Console.WriteLine();
+}
+
+
---
+++ exxMatrizPrinc/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] quadBinarios = new int[5,5];
+
+
+for(int i = 0; i < 5; i++ )
+{
+    for(int j = 0; j < 5; j++)
+    {
+        if(i == j)
+        {
+            quadBinarios[i,j] = 1;
+        }
+        else
+        {
+            quadBinarios[i,j] = 0;
+        }
+    }
+}
+
+for(int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+        System.Console.Write($"{quadBinarios[i, j]}\t ");//Não esquecer do espaço e do CW vazio
+    }
+    System.Console.WriteLine();
+}
