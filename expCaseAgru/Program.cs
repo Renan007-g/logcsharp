67f2a28738a6faec631d97eeeee872f1e8ecdd68
Renan007-g <renan.oliv.2007@gmail.com>
2023-10-03 15:21:48 -0300

    commit diario

---
+++ expCaseAgru/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+string mes = "";
+
+System.Console.Write("Digite o nome de um mes: ");
+mes = Console.ReadLine();
+
+switch(mes)
+{
+    case "fevereiro":
+        System.Console.WriteLine($"O mes de {mes} possui 28 ou 29 dias.");
+        break;
+
+    case "abril":
+    case "junho":
+    case "setembro":
+    case "novembro":
+        System.Console.WriteLine($"O mes de {mes} possui 30 dias.");
+        break;
+
+    case "janeiro":
+    case "março":
+    case "maio":
+    case "julho":
+    case "agosto":
+    case "outubro":
+    case "dezembro":
+        System.Console.WriteLine($"O mes de {mes} possui 31 dias.");
+        break;
+
+    default:
+        System.Console.WriteLine("O valor digitado não corresponde a nenhum mes");
+        break;
+
+}
---
+++ exxCalculadora/Program.cs
@@ -0,0 +1,68 @@
+﻿string tipo;
+int numA;
+int numB;
+
+
+System.Console.WriteLine("Digite qual tipo da operação(soma, subtração, divisão e multiplicação.)");
+tipo = Console.ReadLine();
+
+tipo = tipo.ToLower();
+
+switch(tipo)
+{
+    case "soma":
+        System.Console.WriteLine("Digite a parcela: ");
+        numA = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Digite a outra parcela: ");
+        numB = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    case "subtração":
+        System.Console.WriteLine("Digite o minuendo: ");
+        numA = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Digite o subtraendo: ");
+        numB = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    case "divisão":
+        System.Console.WriteLine("Digite o dividendo: ");
+        numA = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Digite o divisor: ");
+        numB = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    case "multiplição":
+        System.Console.WriteLine("Digite o multiplicando: ");
+        numA = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Digite o multiplicador: ");
+        numB = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    default:
+        System.Console.WriteLine("O tipo de operação está incorreto");
+        break;
+}
+
+
+switch(tipo)
+{
+    case "soma":
+        System.Console.WriteLine($"A subtração entre {numA} + {numB} = {numA - numB}");
+        break;
+
+    case "subtração":
+        System.Console.WriteLine($"A subtração entre {numA} - {numB} = {numA - numB}");
+        break;
+
+    case "divisão":
+        System.Console.WriteLine($"A divisão entre {numA} / {numB} = {numA / numB}");
+        break;
+
+    case "multiplição":
+        System.Console.WriteLine($"A multiplicação entre {numA} * {numB} = {numA * numB}");
+        break;
+
+    default:
+        System.Console.WriteLine("O tipo de operação está incorreto");
+        break;
+}
